ebf82c88c93c305b58bf7e96016e24b50c7e7e31
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-10 15:49:12 +0700

    Tugas 26 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,27 @@
+function panggilIsFinite(){
+  var a = isFinite(2);
+  var b = isFinite(39);
+  var c = isFinite(76);
+  var d = isFinite(50);
+  var e = isFinite(9);
+  var f = isFinite(7);
+  var g = isFinite(41);
+  var h = isFinite(2);
+  var i = isFinite(24);
+  var j = isFinite(1);
+  var k = isFinite(16);
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+  console.log(e);
+  console.log(f);
+  console.log(g);
+  console.log(h);
+  console.log(i);
+  console.log(j);
+  console.log(k);
+}
+// 2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16
+panggilIsFinite()
